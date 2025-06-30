6d1aade6bd14ba89ec8ba22af839949e6964f69a
tpals213 <tpals213@gmail.com>
2025-06-30 13:41:50 +0900

    라우터 통신 테스트

--- src/pages/TestRouter/TestRouter.jsx
+++ src/pages/TestRouter/TestRouter.jsx
@@ -5,24 +5,21 @@ import axios from 'axios';
 
 const TestRouter = () => {
 
-    const connect = () => {
+    const connect = async () => {
         const formData = {
-            name : "장세민"
-        }
+            name: "장세민",
+        };
         try {
-            const response = axios.post(
-                `${process.env.REACT_APP_FASTAPI_ADS_URL}/ads/test/router`,
+            const response = await axios.post(
+                `${process.env.REACT_APP_FASTAPI_BASE_URL}/test/router`,
                 formData
             );
-            console.log(response.data)
-        }
-        catch(err){
+            console.log(response.data.name); // ✅ 응답 본문은 `.data` 안에 들어 있음
+        } catch (err) {
             console.error("저장 중 오류 발생:", err);
         }
-        finally{
+    };
 
-        }
-    }
 
     return (
         <div>
@@ -36,14 +33,11 @@ const TestRouter = () => {
                         <p>라우터 테스트 페이지 입니다.</p>
                     </div>
                     <div>
-                        <button className='border border-gray-500 px-2 py-2'>
+                        <button onClick={connect} className='border border-gray-500 px-2 py-2'>
                             fastAPI 통신
                         </button>
                     </div>
                 </main>
-
-
-
             </div>
         </div >
     );
