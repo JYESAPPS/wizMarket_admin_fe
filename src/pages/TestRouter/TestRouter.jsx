bdc67dac32d80bd743b589c7a20b7f72df5d0cdf
tpals213 <tpals213@gmail.com>
2025-06-30 13:34:41 +0900

    라우터 테스트

--- src/pages/TestRouter/TestRouter.jsx
+++ src/pages/TestRouter/TestRouter.jsx
@@ -5,6 +5,25 @@ import axios from 'axios';
 
 const TestRouter = () => {
 
+    const connect = () => {
+        const formData = {
+            name : "장세민"
+        }
+        try {
+            const response = axios.post(
+                `${process.env.REACT_APP_FASTAPI_ADS_URL}/ads/test/router`,
+                formData
+            );
+            console.log(response.data)
+        }
+        catch(err){
+            console.error("저장 중 오류 발생:", err);
+        }
+        finally{
+
+        }
+    }
+
     return (
         <div>
             <Header />
@@ -15,6 +34,11 @@ const TestRouter = () => {
                 <main>
                     <div>
                         <p>라우터 테스트 페이지 입니다.</p>
+                    </div>
+                    <div>
+                        <button className='border border-gray-500 px-2 py-2'>
+                            fastAPI 통신
+                        </button>
                     </div>
                 </main>
 
